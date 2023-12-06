9f5d99e9302ba04b3b7e2477fc56d88a911e5eed
LyubenGeorgiev <koko6kata1@gmail.com>
2023-12-06 22:17:24 +0200

    Initial hello world commit

---
+++ cmd/shah/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// swagger:response HelloResponse
+type HelloResponse struct {
+	// in:body
+	Message string `json:"message"`
+}
+
+// swagger:route GET / hello
+// helloHandler responds with a "Hello, World!" message.
+// Consumes:
+//   - application/json
+//
+// Produces:
+//   - application/json
+//
+// Responses:
+//
+//	200: HelloResponse
+func helloHandler(rw http.ResponseWriter, r *http.Request) {
+	response := HelloResponse{Message: "Hello, World!"}
+	rw.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(rw).Encode(response); err != nil {
+		http.Error(rw, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/", helloHandler)
+
+	fmt.Println("Server is running on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
